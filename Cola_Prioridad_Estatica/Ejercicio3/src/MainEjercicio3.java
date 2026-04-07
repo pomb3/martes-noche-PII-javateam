0de799109aa2d417075e72cc6e3a9f1ff0054b77
Zoe Vicente <zoenereavicente@gmail.com>
2026-04-07 20:25:20 -0300

    Se agrega ejercicio 3 de colas

---
+++ Cola_Prioridad_Estatica/Ejercicio3/src/MainEjercicio3.java
@@ -0,0 +1,28 @@
+import Implementacion.ColaPrioridadEstatica;
+import Interface.ColaPrioridadTDA;
+
+public class MainEjercicio3 {
+    public static void main(String[] args) {
+        ColaPrioridadTDA cola = new ColaPrioridadEstatica();
+
+        cola.InicializarColaPrioridad();
+
+        // EJERCICIO 3 - Hospital
+
+        System.out.println("\n=== HOSPITAL (TRIAGE) ===");
+
+        System.out.println("Orden de llegada: ");
+        cola.AcolarPrioridad(1, 1);   // raspón
+        System.out.println("Paciente número " + cola.Primero() + " con raspón");
+        cola.AcolarPrioridad(2, 50);  // fractura
+        System.out.println("Paciente número " + cola.Primero() + " con fractura") ;
+        cola.AcolarPrioridad(3, 100); // paro cardíaco
+        System.out.println("Paciente número " + cola.Primero() + " con paro cardiaco");
+
+        System.out.println("\nSe atiende primero paciente número " + cola.Primero() + " - Paciente con paro cardiaco" );
+        cola.Desacolar();
+        System.out.println("Se atiende segundo paciente número " + cola.Primero() + " - Paciente con fractura ");
+        cola.Desacolar();
+        System.out.println("Se atiende tercero paciente núçmero " + cola.Primero() + " - Paciente con raspón ");
+    }
+}
